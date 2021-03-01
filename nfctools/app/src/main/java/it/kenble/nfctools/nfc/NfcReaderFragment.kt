6da2796efd351fae5d769db0b68d0ef931cd603d
Francesco Taddia <fctaddia@gmail.com>
2021-03-01 17:05:00 +0100

    Update NfcReaderFragment.kt

--- nfctools/app/src/main/java/it/kenble/nfctools/nfc/NfcReaderFragment.kt
+++ nfctools/app/src/main/java/it/kenble/nfctools/nfc/NfcReaderFragment.kt
@@ -1,30 +1,46 @@
 package it.kenble.nfctools.nfc
 
+import android.util.Log
+import android.os.Bundle
+import android.view.View
+import android.nfc.tech.Ndef
+import android.view.ViewGroup
+import android.nfc.NdefMessage
 import android.content.Context
 import android.nfc.FormatException
-import android.nfc.NdefMessage
-import android.nfc.tech.Ndef
-import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+
 import it.kenble.nfctools.R
-import it.kenble.nfctools.databinding.FragmentReadBinding
 import it.kenble.nfctools.ui.MainActivity
-import kotlinx.android.synthetic.main.fragment_read.view.*
+import it.kenble.nfctools.databinding.FragmentReadBinding
+
 import java.io.IOException
 
+/**
+ * @author Francesco Taddia
+ * @see 'https://github.com/fctaddia/NfcTools'
+ */
 class NfcReaderFragment : DialogFragment() {
 
-    private lateinit var readBind : FragmentReadBinding
-    private var listener : Listener? = null
-    private var message : String? = null
+    // region Variables
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        readBind = DataBindingUtil.inflate(inflater,R.layout.fragment_read,container,false) ; return readBind.root
+    companion object {
+        val TAG: String = NfcReaderFragment::class.java.simpleName
+        fun newInstance(): NfcReaderFragment { return NfcReaderFragment() }
+    }
+
+    private var message: String? = null
+    private var listener: Listener? = null
+    private lateinit var readBind: FragmentReadBinding
+
+    // endregion
+
+    // region Lifecycle
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        readBind = FragmentReadBinding.inflate(layoutInflater)
+        return readBind.root
     }
 
     override fun onAttach(context: Context) {
@@ -33,9 +49,16 @@ class NfcReaderFragment : DialogFragment() {
         listener!!.onDialogDisplayed()
     }
 
-    override fun onDetach() { super.onDetach() ; listener!!.onDialogDismissed() }
+    override fun onDetach() {
+        super.onDetach()
+        listener!!.onDialogDismissed()
+    }
 
-    fun onNfcDetected(ndef: Ndef) { readFromNFC(ndef) }
+    // endregion
+
+    // region Nfc
+
+    internal fun onNfcDetected(ndef: Ndef) { readFromNFC(ndef) }
 
     private fun readFromNFC(ndef: Ndef) {
         try {
@@ -44,18 +67,16 @@ class NfcReaderFragment : DialogFragment() {
             message = String(ndefMessage.records[0].payload)
             Log.d(TAG, "readFromNFC: $message")
             ndef.close()
-            readBind.root.tv_message!!.text = message
+            readBind.tvMessage.text = message
         } catch (e: IOException) {
             e.printStackTrace()
-            readBind.root.tv_message!!.text = getString(R.string.message_read_error)
+            readBind.tvMessage.text = getString(R.string.message_read_error)
         } catch (e: FormatException) {
             e.printStackTrace()
-            readBind.root.tv_message!!.text = getString(R.string.message_read_error)
+            readBind.tvMessage.text = getString(R.string.message_read_error)
         }
     }
 
-    companion object {
-        val TAG: String = NfcReaderFragment::class.java.simpleName
-        fun newInstance(): NfcReaderFragment { return NfcReaderFragment() }
-    }
+    // endregion
+
 }
