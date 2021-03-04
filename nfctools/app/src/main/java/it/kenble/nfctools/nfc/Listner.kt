b4344ed348a68399fc81c8e2519a373f23cde9ac
Francesco Taddia <fctaddia@gmail.com>
2021-03-04 16:37:58 +0100

    Update Listner.kt

--- nfctools/app/src/main/java/it/kenble/nfctools/nfc/Listner.kt
+++ nfctools/app/src/main/java/it/kenble/nfctools/nfc/Listner.kt
@@ -5,6 +5,15 @@ package it.kenble.nfctools.nfc
  * @see 'https://github.com/fctaddia/NfcTools'
  */
 interface Listener {
+    
+    /**
+     * Show NFC Dialog Reader
+     */
     fun onDialogDisplayed()
+    
+    /**
+     * Dismiss NFC Dialog reader
+     */
     fun onDialogDismissed()
+    
 }
